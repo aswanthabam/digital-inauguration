4094f65259854a571f4171cc48ea8e09d5e6144f
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-09 01:28:51 +0530

    refactor : formatted code

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-var i = 0;
-var audio = new Audio("assets/Inauguration Background Music.mp3");
+var i = 0; // variable to keep track of the number of keypresses
+var audio = new Audio("assets/Inauguration Background Music.mp3"); // audio for background music
+
+/*
+  function to remove hidden class from the element given, 
+  i.e to display an element
+*/
+
 function first(cl) {
-  console.log(document.getElementsByClassName(cl)[0]);
   document.getElementsByClassName(cl)[0].classList.remove("hidden");
-  // visible();
 }
+
 addEventListener("keypress", (event) => {
   visible();
 });
+
+addEventListener("mousedown", (event) => {
+  visible();
+});
+
+/*
+  function to display the elements one by one on keypress
+*/
+
 function visible() {
   if (i == 0) audio.play();
   if (i == 1) first("fire-one");
@@ -20,13 +34,9 @@ function visible() {
       document.getElementsByClassName("content")[0].classList.remove("hidden");
       document.getElementsByClassName("villaku")[0].classList.add("done");
     }, 500);
+    setTimeout(() => {
+      audio.pause();
+    }, 6000);
   }
   i++;
-  // // if (i == 6) first("fire-six");
-  // if (i == 7) {
-  //   first("fire-seven");
-
-  // }
 }
-
-// setInterval(visible, 1000);
